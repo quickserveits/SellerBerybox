5ebdecfbe73c89a35f463232216dd7b2c680d35a
Shahriar Shubho <shubha.shahriar@gamil.com>
2021-07-20 10:38:15 +0600

    busniss imgage

--- src/Components/AddBusiness/AddBusiness.js
+++ src/Components/AddBusiness/AddBusiness.js
@@ -7,12 +7,16 @@ const AddBusiness = () => {
     handleSubmit,
     formState: { errors },
   } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = (data) => {
+    alert("Register Completed")
+    console.log(data)};
   return (
     <div>
       <h3>Add your Business to our page and become berrybox partners</h3>
       <div className="container">
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="row">
+          <div className="col-md-6">
+          <form onSubmit={handleSubmit(onSubmit)}>
           <strong className="mt-3">Business Name : </strong>
           <br />
           <input
@@ -20,7 +24,7 @@ const AddBusiness = () => {
             id="name"
             className="rounded border-success w-100 "
             placeholder="Enter your Business name"
-            ref={register("name", { required: true })}
+            ref={register({ required: true })}
           />{" "}
           <br />
           {errors.name && (
@@ -34,10 +38,10 @@ const AddBusiness = () => {
             id="owner"
             className="rounded border-success w-100"
             placeholder="Enter owner name"
-            ref={register("owner", { required: true })}
+            ref={register({ required: true })}
           />
           <br />
-          {errors.email && (
+          {errors.owner && (
             <span className="text-danger">Owner field is required</span>
           )}
           <br />
@@ -79,7 +83,11 @@ const AddBusiness = () => {
             <option value="female">Jewelry</option>
             <option value="male">Clothes</option>
             <option value="other">Shows</option>
-          </select>{" "}
+          </select>
+          <br />
+          {errors.select && (
+            <span className="text-danger">Add a phone number</span>
+          )}
           <br /> <br />
           <h3>Additional Information</h3>
           <input
@@ -114,6 +122,12 @@ const AddBusiness = () => {
           By Registering your Business to the Site you agree with BeryBox
           Partner Terms and Conditions
         </strong>
+          </div>
+          <div className="com-md-5 pl-5">
+            <img src="https://images.unsplash.com/photo-1586880244386-8b3e34c8382c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZSUyMGNvbW1lcmNlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" alt="" />
+          </div>
+        </div>
+        
       </div>
     </div>
   );
