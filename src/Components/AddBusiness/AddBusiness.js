3c56097789abce5f93a5d3c466c3e44d1620c9e9
shravan kashyap <shravan@smilebots.com>
2021-07-20 19:59:00 +0530

    updated code

--- src/Components/AddBusiness/AddBusiness.js
+++ src/Components/AddBusiness/AddBusiness.js
@@ -12,24 +12,19 @@ const AddBusiness = () => {
     console.log(data);
   };
   return (
-    <div
-    // style={{
-    //   backgroundImage:
-    //     "URL(https://images.unsplash.com/photo-1586880244386-8b3e34c8382c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZSUyMGNvbW1lcmNlfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60)",
-    // }}
-    >
+    <div>
       <h3>Add your Business to our page and become berrybox partners</h3>
       <div className="container ">
         <div className="row">
-          <div className="col-md-5 frame p-4 ml-0" style={{ height: "auto" }}>
+          <div
+            className="col-md-5 frame p-4 ml-0 text-white"
+            style={{ height: "auto", background: "rgba(11, 57, 99, 255)" }}
+          >
             <form onSubmit={handleSubmit(onSubmit)}>
-              {/* <label htmlFor="fullname" className="text-start ">
-                Business Name
-              </label> */}
               <input
                 name="name"
                 id="name"
-                className="form-styling"
+                className="form-styling text-white p-2"
                 placeholder="Enter your Business name"
                 ref={register({ required: true })}
               />{" "}
@@ -40,7 +35,7 @@ const AddBusiness = () => {
               <input
                 name="owner"
                 id="owner"
-                className="form-styling"
+                className="form-styling text-white p-3"
                 placeholder="Enter owner name"
                 ref={register({ required: true })}
               />
@@ -51,7 +46,7 @@ const AddBusiness = () => {
               <input
                 name="address"
                 id="address"
-                className="form-styling"
+                className="form-styling text-white p-3"
                 placeholder="Enter Your Address"
                 ref={register({ required: true })}
               />
@@ -63,16 +58,15 @@ const AddBusiness = () => {
                 name="mobile"
                 type="number"
                 id="mobile"
-                className="form-styling"
+                className="form-styling text-white p-3"
                 placeholder="Enter Your Phone Number"
                 ref={register({ required: true })}
               />
               {errors.mobile && (
                 <span className="text-danger">Add a phone number</span>
               )}
-              {/* <strong className="mt-3"> Select a Category:</strong> */}
               <select
-                className="form-styling"
+                className="form-styling text-white "
                 {...register("Select a Category")}
               >
                 <option value="">--Select Category---</option>
@@ -87,32 +81,38 @@ const AddBusiness = () => {
               <input
                 name="email"
                 id="email"
-                className="form-styling"
+                className="form-styling text-white p-3"
                 placeholder="Enter your email"
                 ref={register("email")}
               />{" "}
               <input
                 name="payment"
                 id="payment"
-                className="form-styling"
+                className="form-styling text-white p-3"
                 placeholder="Enter your payment"
                 ref={register("payment")}
               />{" "}
               <input
                 name="timing"
                 id="timing"
-                className="form-styling"
+                className="form-styling text-white p-3"
                 placeholder="Enter your timing"
                 ref={register("timing")}
               />
-              <button className="btn btn-success mr-5" type="submit">
+              <button
+                className="btn mr-5 text-white"
+                type="submit"
+                style={{ background: "#ebb2c1" }}
+              >
                 Register
               </button>
             </form>
-            <strong>
-              By Registering your Business to the Site you agree with BeryBox
-              Partner Terms and Conditions
-            </strong>
+            <div className="pt-5">
+              <strong>
+                By Registering your Business to the Site you agree with BeryBox
+                Partner Terms and Conditions
+              </strong>
+            </div>
           </div>
           <div className="com-md-8">
             <img
